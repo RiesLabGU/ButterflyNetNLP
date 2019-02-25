2dcbfd2a0a2aad9b3e5f4b73d35e71fc73d75890
vmshirey <vms55@georgetown.edu>
2019-02-25 15:00:17 -0500

    -additional text scrubbing

--- OCR/extractor.py
+++ OCR/extractor.py
@@ -10,11 +10,16 @@ printable = set(string.printable)
 raw = parser.from_file('sample.pdf')
 text = raw['content']
 text = text.lower().replace('\n', ' ').replace('\r', ' ').replace('\t', ' ').strip()
+text = ' '.join(text.split())
 text = re.sub(r'(?<=[a-z]{4})[.]', '\n', text)
 text = text.encode('ascii', errors='ignore').decode(encoding='utf8').replace(r'\\', '').replace('"', '')
 text = re.sub(r'(?<=nae)', '\n', text)
 text = re.sub(r'(?<=dae)', '\n', text)
-text = text.replace('life histories of cascadia butterflies', '')
+text = text.replace('life histories of cascadia butterflies', '').replace('\\', '').replace('|', '')
+text = re.sub(r'([0-9]){3}', '', text)
+text = re.sub(r'([i])\b', '', text)
+text = text.replace('-', ' ')
+
 
 #line cleaing
 
