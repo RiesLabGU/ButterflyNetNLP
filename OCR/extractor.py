86e149a78527f38d056feb7af7efebde0d1cc365
vmshirey <vms55@georgetown.edu>
2019-02-25 14:03:30 -0500

    -added OCR -> txt extractor

--- OCR/extractor.py
+++ OCR/extractor.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import argparse
+from tika import parser
 
+def main(data_file):
+    raw = parser.from_file(data_file)
 
+    with open("raw.txt") as f:
+        f.write(raw['content'])
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='habitat-phrases')
+    parser = argparse.ArgumentParser(description='pdf to convert')
     parser.add_argument('--path', type=str, default="sample.pdf",
                         help='path to the menu to update')
     args = parser.parse_args()
