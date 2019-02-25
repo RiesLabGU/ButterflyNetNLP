1f98116f52bbd16976e956ce37617d89956a7325
vmshirey <vms55@georgetown.edu>
2019-02-25 14:45:55 -0500

    -more text scrubbing, regex

--- OCR/extractor.py
+++ OCR/extractor.py
@@ -6,11 +6,17 @@ from tika import parser
 
 printable = set(string.printable)
 
+# regex cleaning and line setting
 raw = parser.from_file('sample.pdf')
 text = raw['content']
 text = text.lower().replace('\n', ' ').replace('\r', ' ').replace('\t', ' ').strip()
 text = re.sub(r'(?<=[a-z]{4})[.]', '\n', text)
 text = text.encode('ascii', errors='ignore').decode(encoding='utf8').replace(r'\\', '').replace('"', '')
+text = re.sub(r'(?<=nae)', '\n', text)
+text = re.sub(r'(?<=dae)', '\n', text)
+text = text.replace('life histories of cascadia butterflies', '')
+
+#line cleaing
 
 with open("raw.txt", 'w', encoding='utf8') as f:
     f.write(text)
